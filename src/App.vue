4165c35c69fd2113455ffb5ce4b87d1a175b9ff1
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-10-16 06:51:47 +0700

    feat: v-model

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,17 @@
 export default {
   data() {
     return {
-      count: 0,
       message: "Hello Nanda!",
     };
-  },
-  methods: {
-    increment() {
-      this.count += 2;
-    },
   },
 };
 </script>
 
 <template>
-  <div>Message is: {{ message }}</div>
-  <br />
-  <h1 :title="message">What's the message ? (Hover)</h1>
-  <br />
-  <h1>{{ count }}</h1>
-  <button @click="count++">+ 1</button>
-  <button @click="increment">Increment 2 by methods</button>
+  <h1>{{ message }}</h1>
+  <input v-model="message" />
+  <!-- same with belows -->
+  <input :value="message" @input="message = $event.target.value" />
 </template>
 
 <style scoped></style>
