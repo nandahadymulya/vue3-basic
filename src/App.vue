1ca9b13b638b668760d28b94a04fee1433e21cb4
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-10-16 07:32:33 +0700

    feat: v-once

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import ChildComponent from "./components/ChildComponent.vue";
 
 export default {
   data() {
     return {
-      message: "First Message",
+      message: "Render Only One",
     };
-  },
-
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-    console.log(document.getElementById("lifecycle"));
-    // console.log(document.getElementById("lifecycle").textContent);
-    // fetch data from API
-  },
-  // after created component
-  beforeMount() {
-    console.log("beforeMount");
-  },
-  mounted() {
-    console.log("mounted");
-    console.log(document.getElementById("lifecycle"));
-    console.log(document.getElementById("lifecycle").textContent);
-    // process data
-  },
-  // re-render component
-  beforeUpdate() {
-    console.log("beforeUpdate");
-    console.log(document.getElementById("lifecycle").textContent);
-  },
-  updated() {
-    console.log("updated");
-    console.log(document.getElementById("lifecycle").textContent);
-  },
-  // destroy component after move to component
-  beforeUnmount() {
-    console.log("beforeUnmount");
-  },
-  unmounted() {
-    console.log("unmounted");
   },
 };
 </script>
 
 <template>
-  <h1>Parent App</h1>
-  <p id="lifecycle">{{ message }}</p>
-  <button @click="message = 'Re-rendering is running this.'">Updated</button>
+  <h1 v-once>{{ message }}</h1>
+  <button @click="message = 'Rendering again.'">V-Once</button>
 </template>
 
 <style scoped></style>
