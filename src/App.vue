4abd555b61e7678bbd1f620b5d090800b4514caa
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-10-16 08:41:23 +0700

    feat: update parent from child 2

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,7 @@ export default {
 
 <template>
   <h1>Event: {{ message }}</h1>
-  <hr />
-  <ChildComponent :model-value="message" @update:model-value="message = $event" />
-
+  <!-- <ChildComponent :model-value="message" @update:model-value="message = $event" /> -->
   <hr />
   <ChildComponent v-model="message" />
 </template>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,9 +1,21 @@
 <script>
 export default {
   props: ["modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
 };
 </script>
 <template>
   <h2>Child Component:</h2>
-  <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+  <!-- <input :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" /> -->
+
+  <input v-model="value" type="text" />
 </template>
