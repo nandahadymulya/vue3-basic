5c7050c0059d978c51799201fe0eda93c6d6553e
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-10-16 07:11:35 +0700

    feat: props from data script

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,17 @@ import ChildComponent from "./components/ChildComponent.vue";
 
 export default {
   components: { ChildComponent },
+  data() {
+    return {
+      message: "Hello from props data!",
+    };
+  },
 };
 </script>
 
 <template>
   <h1>Parent App</h1>
-  <ChildComponent message="Nanda" />
+  <ChildComponent :message="message" />
   <child-component message="Hady" />
 </template>
 
